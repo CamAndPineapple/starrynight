e69960da0c37bd8091c71f471d3ed07a7d2d7809
Camden Rudisill <camden.rudisill@richmond.edu>
2015-09-15 21:24:31 -0400

    Add: demo api

--- src/js/main.js
+++ src/js/main.js
@@ -4,6 +4,7 @@ $(function() {
 	var $city = $("#city");
 	var $state = $("#state");
 	var $country = $("#country");
+	var $temp = $("#temp");
 
 	$(".search-form").on("submit", function(event) {
 		event.preventDefault();
@@ -15,9 +16,11 @@ $(function() {
 				var city = parsed_json['location']['city'];
 				var state = parsed_json['location']['state'];
 				var country = parsed_json['location']['country_name'];
+				var temp =  parsed_json['forecast']['simpleforecast']['forecastday'][0]['high']['fahrenheit'];
 				$city.append(city);
 				$state.append(state);
 				$country.append(country);
+				$temp.append(temp);
 
 			});
 	});
