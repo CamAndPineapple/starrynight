21de3e25d7d8b16551ab388069057f2eb38d519f
Camden Rudisill <camden.rudisill@richmond.edu>
2015-09-25 23:51:29 -0400

    Add: weighted avg functions working | clean

--- app/components/SearchBar.js
+++ app/components/SearchBar.js
@@ -50,103 +50,84 @@ export default React.createClass({
 			clouds: " ",
 			forecastMessage: " "
 		});
+		// "https://api.wunderground.com/api/" + API_KEY + "/geolookup/conditions/astronomy/forecast/q/" + state + "/" + city + ".json"
 	},
 
 	searchbarSearch: function(e) {
-		var self = this;
 		e.preventDefault();
 		var API_KEY = "42e0777a5e56eeaf";
-		var API_KEY_DS = "f";
+		var API_KEY_FIO = "e9a70b3c8567afd2b17b50b9699f6a24";
+		var self = this;
 		var inputValue = document.getElementById("searchTextField").value;
+		var cloudPercentage;
+		var cloudWeighted;
+		var illumPercentage;
+		var illumWeighted;
+		var cloudWeight = 0.80;
+		var illumWeight = 0.20;
+
 		var city = inputValue.split(", ")[0];
 		var state = inputValue.split(", ")[1];
 		
+		this.setState({
+			searchbarValue: inputValue
+		});
+
 		$.ajax({
 			type: "get",
 			url: "https://api.wunderground.com/api/" + API_KEY + "/geolookup/conditions/astronomy/forecast/q/" + state + "/" + city + ".json",
 			dataType: "jsonp",
 			success: function(parsed_json) {
-
-				var moonPhaseVar = parsed_json['moon_phase']['phaseofMoon'];
-				var moonAgeVar = parsed_json['moon_phase']['ageOfMoon'];
-				var moonPhaseClipped = moonPhaseVar.split(' ').join('');
-				var illuminationPercentage = parsed_json['moon_phase']['percentIlluminated'];
-		
-				self.setState({
-						searchbarValue: inputValue,
-						showTestContainerWrapper: true,
-						showTitleContainer: false,
-						city: parsed_json["location"]["city"] + ',',
-						state: parsed_json["location"]["state"],
-						illumination: parsed_json['moon_phase']['percentIlluminated'],
-						moonPhase: parsed_json['moon_phase']['phaseofMoon'],
-						moonAge: parsed_json['moon_phase']['ageOfMoon'],
-						img: self.arrayOfMoonPhaseImg[moonPhaseClipped]
-				});
-			}
-		});
-		$.ajax({
-			type: "get",
-			url: 'http://api.openweathermap.org/data/2.5/forecast/' + city + '?id=524901&APPID=1111111111',
-			success: function(parsed_json) {
-
-				var cloudPercentage = parsed_json['list'][0]['clouds']['all'];
-				var message;
-
-				
-
-				if (cloudPercentage >= 80) {
-					message = self.arrayOfForecastMessages["horrible"];
-				} else if (cloudPercentage >= 60) {
-					message = self.arrayOfForecastMessages["lousy"];
-				} else if (cloudPercentage >= 40) {
-					message = self.arrayOfForecastMessages["neutral"];
-				} else if (cloudPercentage >= 10) {
-					message = self.arrayOfForecastMessages["good"];
-				} else if (cloudPercentage >= 0) {
-					message = self.arrayOfForecastMessages["perfect"];
-				} 
-
-				self.setState({
-						clouds: cloudPercentage,
-						forecastMessage: message
-				});
-
-				
-			}
-		});
-
-
-		
-	},
-	clickGlobeForLocation: function() {
-		var API_KEY = "42e0777a5e56eeaf";
-		var API_KEY_FIO = "e9a70b3c8567afd2b17b50b9699f6a24";
-		var self = this;
-		$.ajax({
-			type: "get",
-			url: "https://api.wunderground.com/api/" + API_KEY + "/geolookup/q/autoip.json",
-			dataType: "jsonp",
-			success: function(parsed_json) {
 				var LATITUDE = parsed_json["location"]["lat"];
 				var LONGITUDE = parsed_json["location"]["lon"];
 
-				$.when(
+				$.ajax({
+					type: "get",
+					url: "https://api.forecast.io/forecast/" + API_KEY_FIO + "/" + LATITUDE + "," + LONGITUDE,
+					dataType: "jsonp",
+					success: function(parsed_json) {
 
-					$.get("https://api.forecast.io/forecast/" + API_KEY_FIO + "/" + LATITUDE + "," + LONGITUDE, function(parsed_json) {
+
 						// get cloud cover which comes as a decimal between 0 and 1
-						var cloudCoverage = 100 * parsed_json["currently"]["cloudCover"];
-						alert(cloudCoverage);
+						cloudPercentage = 100 * parsed_json["currently"]["cloudCover"];
+
+						if (cloudPercentage >= 80) {
+							cloudWeighted = 1 * cloudWeight;
+						} else if (cloudPercentage >= 60) {
+							cloudWeighted = 2 * cloudWeight;
+						} else if (cloudPercentage >= 40) {
+							cloudWeighted = 3 * cloudWeight;
+						} else if (cloudPercentage >= 10) {
+							cloudWeighted = 4 * cloudWeight;
+						} else if (cloudPercentage >= 0) {
+							cloudWeighted = 5 * cloudWeight;
+						}
 
 
-					}),
+						self.setState({
+							clouds: cloudPercentage
+						});
+
+					}
 
 
-					$.get("https://api.wunderground.com/api/" + API_KEY + "/geolookup/conditions/astronomy/forecast/q/" + LATITUDE + "," + LONGITUDE + ".json", function(parsed_json) {
+
+				});
+
+
+
+				$.ajax({
+					type: "get",
+					url: "https://api.wunderground.com/api/" + API_KEY + "/geolookup/conditions/astronomy/forecast/q/" + LATITUDE + "," + LONGITUDE + ".json",
+					dataType: "jsonp",
+					success: function(parsed_json) {
+
 						var moonPhaseVar = parsed_json['moon_phase']['phaseofMoon'];
 						var moonAgeVar = parsed_json['moon_phase']['ageOfMoon'];
 						var moonPhaseClipped = moonPhaseVar.split(' ').join('');
-						var illuminationPercentage = parsed_json['moon_phase']['percentIlluminated'];
+
+						illumPercentage = parsed_json['moon_phase']['percentIlluminated'];
+
 
 						self.setState({
 							searchbarValue: inputValue,
@@ -159,20 +140,192 @@ export default React.createClass({
 							moonAge: parsed_json['moon_phase']['ageOfMoon'],
 							img: self.arrayOfMoonPhaseImg[moonPhaseClipped]
 						});
-					})
 
 
-				).then(function() {
-					alert("finished!");
+						if (illumPercentage >= 80) {
+							illumWeighted = 1 * illumWeight
+						} else if (illumPercentage >= 60) {
+							illumWeighted = 2 * illumWeight
+						} else if (illumPercentage >= 40) {
+							illumWeighted = 3 * illumWeight
+						} else if (illumPercentage >= 10) {
+							illumWeighted = 4 * illumWeight
+						} else if (illumPercentage >= 0) {
+							illumWeighted = 5 * illumWeight
+						}
 
 
-
+					}
 				});
+
+
+			}
+		});
+
+setTimeout(function(){ 
+			var weightedAvg = cloudWeighted + illumWeighted;
+			var msg;
+
+			console.log(cloudWeighted);
+			console.log(illumWeighted);
+
+
+			if (weightedAvg >= 4) {
+				msg = self.arrayOfForecastMessages["perfect"];
+			} else if (weightedAvg >= 3) {
+				msg = self.arrayOfForecastMessages["good"];
+			} else if (weightedAvg >= 2) {
+				msg = self.arrayOfForecastMessages["neutral"];
+			} else if (weightedAvg >= 1) {
+				msg = self.arrayOfForecastMessages["lousy"];
+			} else if (weightedAvg >= 0) {
+				msg = self.arrayOfForecastMessages["horrible"];
 			}
 
+			self.setState({
+			
+				forecastMessage: msg
+			});
 
 
-		});
+
+}, 3000);
+
+},
+	clickGlobeForLocation: function() {
+		var API_KEY = "42e0777a5e56eeaf";
+		var API_KEY_FIO = "e9a70b3c8567afd2b17b50b9699f6a24";
+		var self = this;
+		var inputValue = document.getElementById("searchTextField").value;
+		var cloudPercentage;
+		var cloudWeighted;
+		var illumPercentage;
+		var illumWeighted;
+		
+
+		$.ajax({
+			type: "get",
+			url: "https://api.wunderground.com/api/" + API_KEY + "/geolookup/q/autoip.json",
+			dataType: "jsonp",
+			success: function(parsed_json) {
+				var LATITUDE = parsed_json["location"]["lat"];
+				var LONGITUDE = parsed_json["location"]["lon"];
+
+				$.ajax({
+					type: "get",
+					url: "https://api.forecast.io/forecast/" + API_KEY_FIO + "/" + LATITUDE + "," + LONGITUDE,
+					dataType: "jsonp",
+					success: function(parsed_json) {
+
+						
+								// get cloud cover which comes as a decimal between 0 and 1
+								cloudPercentage = 100 * parsed_json["currently"]["cloudCover"];
+
+						if (cloudPercentage >= 80) {
+							cloudWeighted = 1 * cloudWeight;
+						} else if (cloudPercentage >= 60) {
+							cloudWeighted = 2 * cloudWeight;
+						} else if (cloudPercentage >= 40) {
+							cloudWeighted = 3 * cloudWeight;
+						} else if (cloudPercentage >= 10) {
+							cloudWeighted = 4 * cloudWeight;
+						} else if (cloudPercentage >= 0) {
+							cloudWeighted = 5 * cloudWeight;
+						}
+
+
+						self.setState({
+							clouds: cloudPercentage
+						});
+
+							}
+
+					
+
+
+						});
+
+				
+
+
+
+
+				$.ajax({
+					type: "get",
+					url: "https://api.wunderground.com/api/" + API_KEY + "/geolookup/conditions/astronomy/forecast/q/" + LATITUDE + "," + LONGITUDE + ".json",
+					dataType: "jsonp",
+					success: function(parsed_json) {
+
+						var moonPhaseVar = parsed_json['moon_phase']['phaseofMoon'];
+						var moonAgeVar = parsed_json['moon_phase']['ageOfMoon'];
+						var moonPhaseClipped = moonPhaseVar.split(' ').join('');
+						
+						illumPercentage = parsed_json['moon_phase']['percentIlluminated'];
+
+
+						self.setState({
+							searchbarValue: inputValue,
+							showTestContainerWrapper: true,
+							showTitleContainer: false,
+							city: parsed_json["location"]["city"] + ',',
+							state: parsed_json["location"]["state"],
+							illumination: parsed_json['moon_phase']['percentIlluminated'],
+							moonPhase: parsed_json['moon_phase']['phaseofMoon'],
+							moonAge: parsed_json['moon_phase']['ageOfMoon'],
+							img: self.arrayOfMoonPhaseImg[moonPhaseClipped]
+						});
+						
+
+						if (illumPercentage >= 80) {
+							illumWeighted = 1 * illumWeight
+						} else if (illumPercentage >= 60) {
+							illumWeighted = 2 * illumWeight
+						} else if (illumPercentage >= 40) {
+							illumWeighted = 3 * illumWeight
+						} else if (illumPercentage >= 10) {
+							illumWeighted = 4 * illumWeight
+						} else if (illumPercentage >= 0) {
+							illumWeighted = 5 * illumWeight
+						}
+
+						
+							}
+						});
+
+		
+}})
+
+setTimeout(function(){ 
+			var weightedAvg = cloudWeighted + illumWeighted;
+			var msg;
+
+
+
+			if (weightedAvg >= 0) {
+				msg = self.arrayOfForecastMessages["horrible"];
+			} else if (weightedAvg >= 1) {
+				msg = self.arrayOfForecastMessages["lousy"];
+			} else if (weightedAvg >= 2) {
+				msg = self.arrayOfForecastMessages["neutral"];
+			} else if (weightedAvg >= 3) {
+				msg = self.arrayOfForecastMessages["good"];
+			} else if (weightedAvg >= 4) {
+				msg = self.arrayOfForecastMessages["perfect"];
+			}
+
+			self.setState({
+			
+				forecastMessage: msg
+			});
+
+
+
+}, 3000);
+
+
+},
+
+		
 
 
 
@@ -203,7 +356,7 @@ export default React.createClass({
 
 
 
-	},
+
 	
 	render: function() {
 		return (
